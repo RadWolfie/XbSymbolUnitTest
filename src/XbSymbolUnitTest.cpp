f41ea7403dedc540f514f3bc4e1e5d1de98ea825
RadWolfie <RadWolfie@users.noreply.github.com>
2019-12-16 00:16:33 -0600

    Update to use XbSymbolDatabase API v3

--- src/XbSymbolUnitTest.cpp
+++ src/XbSymbolUnitTest.cpp
@@ -79,6 +79,7 @@ int output_result_XbSDB()
 
 extern void EmuOutputMessage(xb_output_message mFlag, const char *message);
 extern bool VerifyXbeIsBuildWithXDK(const xbe_header *pXbeHeader);
+extern bool VerifyXbSymbolDatabaseFilters(const xbe_header *pXbeHeader);
 
 extern int run_test_raw(const xbe_header *pXbeHeader);
 extern int run_test_virtual(const xbe_header *pXbeHeader,
@@ -96,9 +97,9 @@ int main(int argc, char **argv)
 		path_xbe = argv[1];
 	}
 
-	XbSymbolSetOutputVerbosity(XB_OUTPUT_MESSAGE_DEBUG);
-	XbSymbolSetOutputMessage(EmuOutputMessage);
-	XbSDB_test_error = XbSymbolDataBaseTestOOVPAs();
+	XbSymbolDatabase_SetOutputVerbosity(XB_OUTPUT_MESSAGE_DEBUG);
+	XbSymbolDatabase_SetOutputMessage(EmuOutputMessage);
+	XbSDB_test_error = XbSymbolDatabase_TestOOVPAs();
 
 	// Do not process xbe test verification
 	if (argc == 1) {
@@ -106,11 +107,6 @@ int main(int argc, char **argv)
 		std::cout << "INFO: No xbe given, unit test end.\n";
 		return test_ret;
 	}
-
-	// For output various false detection messages.
-	XbSymbolBypassBuildVersionLimit(true);
-	XbSymbolContinuousSigScan(true);
-	XbSymbolFirstDetectAddressOnly(true);
 
 	std::setlocale(LC_ALL, "English");
 
@@ -132,6 +128,11 @@ int main(int argc, char **argv)
 	    reinterpret_cast<const xbe_header *>(xbe_data);
 
 	if (!VerifyXbeIsBuildWithXDK(pXbeHeader)) {
+		pause_for_user_input();
+		return UNITTEST_FAIL_INVALID_XBE;
+	}
+
+	if (!VerifyXbSymbolDatabaseFilters(pXbeHeader)) {
 		pause_for_user_input();
 		return UNITTEST_FAIL_INVALID_XBE;
 	}
@@ -297,7 +298,8 @@ bool VerifyXbeIsBuildWithXDK(const xbe_header *pXbeHeader)
 	std::wcstombs(tAsciiTitle, pCertificate->wszTitleName, sizeof(tAsciiTitle));
 
 	// Output Symbol Database version
-	std::cout << "XbSymbolLibraryVersion: " << XbSymbolLibraryVersion() << "\n";
+	std::cout << "XbSymbolDatabase_LibraryVersion: "
+	          << XbSymbolDatabase_LibraryVersion() << "\n";
 
 	// Store Certificate Details
 	std::cout << "Name                  : " << tAsciiTitle << "\n";
@@ -327,26 +329,189 @@ bool VerifyXbeIsBuildWithXDK(const xbe_header *pXbeHeader)
 	std::cout.flags(cout_fmt);
 
 	std::cout << "BuildVersion          : " << buildVersion << "\n";
+
+	std::cout << "\n";
+	return true;
+}
+
+
+bool GetXbSymbolDatabaseFilters(const xbe_header *pXbeHeader, bool is_raw,
+                                XbSDBLibraryHeader &library_output,
+                                XbSDBSectionHeader &section_output)
+{
+	std::string error_msg = "unknown";
+
+	library_output.count =
+	    XbSymbolDatabase_GenerateLibraryFilter(pXbeHeader, nullptr);
+
+	if (library_output.count != 0) {
+		library_output.filters = new XbSDBLibrary[library_output.count];
+	}
+	else {
+		error_msg = "library_input.count variable return 0.";
+		goto scanError;
+	}
+
+	if (library_output.filters == nullptr) {
+		error_msg =
+		    "Unable to allocate buffer for library_input.filters variable.";
+		goto scanError;
+	}
+
+	(void)XbSymbolDatabase_GenerateLibraryFilter(pXbeHeader, &library_output);
+
+	section_output.count =
+	    XbSymbolDatabase_GenerateSectionFilter(pXbeHeader, nullptr, is_raw);
+
+	if (section_output.count != 0) {
+		section_output.filters = new XbSDBSection[section_output.count];
+	}
+	else {
+		error_msg = "section_input.count variable return 0.";
+		goto scanError;
+	}
+
+	if (section_output.filters == nullptr) {
+		error_msg =
+		    "Unable to allocate buffer for section_input.filters variable.";
+		goto scanError;
+	}
+
+	(void)XbSymbolDatabase_GenerateSectionFilter(pXbeHeader, &section_output,
+	                                             is_raw);
+	return true;
+
+scanError:
+	if (library_output.filters) {
+		delete[] library_output.filters;
+		library_output.filters = nullptr;
+	}
+
+	if (section_output.filters) {
+		delete[] section_output.filters;
+		section_output.filters = nullptr;
+	}
+
+	std::cout << "ERROR: GetXbSymbolDatabaseFilters failed for: " << error_msg << "\n";
+	pause_for_user_input();
+	return false;
+}
+
+bool VerifyXbSymbolDatabaseFilters(const xbe_header *pXbeHeader)
+{
+	XbSDBLibraryHeader library_input = {};
+	XbSDBSectionHeader section_input = {};
+
+	std::cout << "Getting library and section filters...\n";
+
+	if (!GetXbSymbolDatabaseFilters(pXbeHeader, true, library_input,
+	                                section_input)) {
+		return false;
+	}
+
+	// Verbose both library and section filters.
+	std::cout << "Outputting library filters...\n";
+
+	unsigned int i;
+	for (i = 0; i < library_input.count; i++) {
+		std::stringstream str_format;
+		str_format << "Library[" << i
+		           << "]\n    name    = " << library_input.filters[i].name
+		           << ";\n    build   = " << std::dec
+		           << library_input.filters[i].build_version
+		           << ";\n    qre ver = 0x" << std::hex
+		           << library_input.filters[i].qfe_version
+		           << ";\n    flag    = 0x" << std::hex
+		           << library_input.filters[i].flag << ";\n";
+		std::cout << str_format.str();
+	}
+
+	std::cout << "\nOutputting section filters...\n";
+
+	for (i = 0; i < section_input.count; i++) {
+		std::stringstream str_format;
+		str_format << "Section[" << i
+		           << "]\n     name        = " << section_input.filters[i].name
+		           << ";\n     virt addr   = 0x" << std::hex
+		           << section_input.filters[i].xb_virt_addr
+		           << ";\n     buffer addr = 0x" << std::hex
+		           << section_input.filters[i].buffer_lower
+		           << ";\n     buffer size = 0x" << std::hex
+		           << section_input.filters[i].buffer_size << ";\n";
+		std::cout << str_format.str();
+	}
+
+	delete[] library_input.filters;
+	delete[] section_input.filters;
+
+	std::cout << "\n";
+
 	return true;
 }
 
 void ScanXbe(const xbe_header *pXbeHeader, bool is_raw)
 {
-	bool scan_ret;
+	std::string error_msg = "unknown";
 
 	// Ensure nothing is still in g_SymbolAddresses before new scan process
 	// start.
 	g_SymbolAddresses.clear();
 
-	scan_ret = XbSymbolScan(pXbeHeader, EmuRegisterSymbol, is_raw);
+	XbSymbolContextHandle pHandle;
+	XbSDBLibraryHeader library_input = {};
+	XbSDBSectionHeader section_input = {};
 
-	if (!scan_ret) {
-		std::cout << "ERROR: XbSymbolScan failed!\n";
-		pause_for_user_input();
+	if (!GetXbSymbolDatabaseFilters(pXbeHeader, is_raw, library_input,
+	                                section_input)) {
 		return;
 	}
 
+	xbaddr kt_addr = XbSymbolDatabase_GetKernelThunkAddress(pXbeHeader);
+
+	if (!XbSymbolDatabase_CreateXbSymbolContext(&pHandle, EmuRegisterSymbol,
+	                                            library_input, section_input,
+	                                            kt_addr)) {
+		error_msg = "Unable to create XbSymbolContext handle.";
+		goto scanError;
+	}
+
+	delete[] library_input.filters;
+	library_input.filters = nullptr;
+	delete[] section_input.filters;
+	section_input.filters = nullptr;
+
+	// For output various false detection messages.
+	XbSymbolContext_SetBypassBuildVersionLimit(pHandle, true);
+	XbSymbolContext_SetContinuousSigScan(pHandle, true);
+	XbSymbolContext_SetFirstDetectAddressOnly(pHandle, true);
+
+	XbSymbolContext_ScanManual(pHandle);
+
+	// TODO: Replace below to use XbSymbolContext_ScanLibrary function to speed
+	// up scan process. Either by replace toolset to CLANG or wait until MSVC
+	// support C11 standard. Perhaps use macro to detect compiler selection?
+	// Then toggle between the two.
+	XbSymbolContext_ScanAllLibraryFilter(pHandle);
+
+	XbSymbolContext_RegisterXRefs(pHandle);
+
+	XbSymbolContext_Release(pHandle);
+
 	std::cout << "\n";
+
+	return;
+
+scanError:
+	if (library_input.filters) {
+		delete[] library_input.filters;
+	}
+
+	if (section_input.filters) {
+		delete[] section_input.filters;
+	}
+
+	std::cout << "ERROR: ScanXbe failed for: " << error_msg << "\n";
+	pause_for_user_input();
 }
 
 int run_test_raw(const xbe_header *pXbeHeader)
